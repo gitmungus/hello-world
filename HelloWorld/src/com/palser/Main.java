b408f7ba36bc5dba5b161bf21d375c592d8ae456
Vince <posterfan@gmail.com>
2017-07-30 17:28:02 +0100

    My Java code

--- HelloWorld/src/com/palser/Main.java
+++ HelloWorld/src/com/palser/Main.java
@@ -1,8 +1,49 @@
 package com.palser;
 
+import java.math.BigInteger;
+
 public class Main {
 
-    public static void main(String[] args) {
-	  System.out.println("Hello, world!");// write your code here
+    private static int xxx = 10;
+
+    public static void main(String[] args)
+    {
+        String x = "Hello";
+
+        int y = 0;
+
+        ++y;
+
+        System.out.println(y);
+        fff    = "Goodbye";
+
+        boolean value = true;
+
+
+        String hi = "hI MATE"; // immutable string
+        hi = hi + "what now"; // a new string!!
+        System.out.println(hi);
+        {
+            String v;
+            v = "Helllo";
+
+
+        }
+
+        {
+            String v = "oioijo";
+
+        }
+
+        String v = "sdsdg";
+
+        BigInteger rrx;
+        rrx = BigInteger.valueOf(10000000);
+
+
+        System.out.println("Hello, world!");// write your code here
+        System.out.println(xxx);// write your code here
+        System.out.println(xxx++);// write your code here
+        System.out.println(++xxx);// write your code here
     }
 }
